1e94db0b0e2f6c8c47098886ac29270b3984ff54
slashinfty <matt.braddock@gmail.com>
2020-04-16 11:01:27 -0400

    working model

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var OWMID, OWMAPI, GCALID, GCALAPI;
-var CALENDAR = 'primary';
+var ZIP, OWMAPI;
 
 $(document).ready(() => {
-  clock();
-  //currentWeather();
-  //forecastWeather();
+  urlCheck();
 });
+
+function urlCheck() {
+  let url = new URL(window.location.href);
+  if (url.searchParams.has('zip') && url.searchParams.has('owmapi')) {
+    ZIP = url.searchParams.get('zip');
+    OWMAPI = url.searchParams.get('owmapi');
+  } else {
+    ZIP = prompt("Please enter ZIP code.");
+    OWMAPI = prompt("Please enter OpenWeatherMap API key.");
+  }
+  clock();
+  currentWeather();
+  forecastWeather();
+  calendar();
+}
 
 function clock() {
   let today = new Date();
@@ -14,10 +26,7 @@ function clock() {
   h = h < 10 ? "0" + h : h;
   let m = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
   let s = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
-  $("#clock").html(h + ':' + m + ':' + s + ampm).textFit({reProcess: true, widthOnly: true, maxFontSize: 400});
-  let date = today.toString().replace(/\s\d\d:.*$/,'');
-  $("#today").text(date).textFit({reProcess: true, widthOnly: true, maxFontSize: 400});
-  $("#day-labels").textFit({reProcess: true, alignHoriz: true});
+  $("#clock").html(h + ':' + m + ':' + s + ampm).textFit({reProcess: true, widthOnly: true, alignVert: true, maxFontSize: 400});
   let t = setTimeout(clock, 500); //0.5 seconds
 }
 
@@ -32,8 +41,14 @@ async function currentWeather() {
     else if (deg <= 292) return 'W';
     else if (deg <= 337) return 'NW';
   }
-  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${OWMID}&units=imperial&appid=${OWMAPI}`);
-  let body = await response.json();
+  let response, body;
+  try {
+    response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${ZIP},us&units=imperial&appid=${OWMAPI}`);
+    body = await response.json();
+  } catch (err) {
+    console.log(err);
+    $("#current").html("Error fetching weather data.")
+  }
   let describe = body.weather[0].description[0].toUpperCase() + body.weather[0].description.slice(1);
   let temp = body.main.temp.toString() + '\xB0' + 'F';
   let feels = body.main.feels_like.toString() + '\xB0' + 'F';
@@ -48,8 +63,14 @@ async function currentWeather() {
 }
 
 async function forecastWeather() {
-  let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${OWMID}&units=imperial&appid=${OWMAPI}`);
-  let body = await response.json();
+  let response, body;
+  try {
+    response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${ZIP},us&units=imperial&appid=${OWMAPI}`);
+    body = await response.json();
+  } catch (err) {
+    console.log(err);
+    $("#forecast").html("Error fetching weather data.")
+  }
   let eightAm = [];
   let twoPm = [];
   let eightPm = [];
@@ -104,5 +125,50 @@ async function forecastWeather() {
     $("#day-" + dayCount + "-3").find(".daily-temp").html("8PM<br>" + day.main.temp.toString() + '\xB0' + 'F').textFit({reProcess: true, widthOnly: true, maxFontSize: 23});
     $("#day-" + dayCount + "-3").find(".daily-icon").html('<img class="daily-icon" src="http://openweathermap.org/img/wn/' + day.weather[0].icon + '@2x.png">');
   });
+  let t = setTimeout(forecastWeather, 3600000); //1 hour
+}
+
+function calendar() {
+  $("#day-labels").find(".day-name").textFit({reProcess: true, alignHoriz: true, alignVert: true});
+  let date = new Date();
+  let year = date.getFullYear();
+  let numberOfDays;
+  let monthName = monthNumber => {
+    switch (monthNumber) {
+      case 0: numberOfDays = 31; return 'JAN';
+      case 1: numberOfDays = year % 4 ? 28 : 29; return 'FEB';
+      case 2: numberOfDays = 31; return 'MAR';
+      case 3: numberOfDays = 30; return 'APR';
+      case 4: numberOfDays = 31; return 'MAY';
+      case 5: numberOfDays = 30; return 'JUN';
+      case 6: numberOfDays = 31; return 'JUL';
+      case 7: numberOfDays = 31; return 'AUG';
+      case 8: numberOfDays = 30; return 'SEP';
+      case 9: numberOfDays = 31; return 'OCT';
+      case 10: numberOfDays = 30; return 'NOV';
+      case 11: numberOfDays = 31; return 'DEC';
+    }
+  }
+  $('#month-year').html(monthName(date.getMonth()) + ' ' + year.toString()).textFit({reProcess: true, alignVert: true});
+  let todayDate = date.getDate();
+  let todayDay = date.getDay();
+  let dayDiff = (todayDate - 1) % 7;
+  let dayOne = todayDay - dayDiff;
+  dayOne = dayOne < 0 ? dayOne += 7 : dayOne;
+  let todayPlace, lastDay;
+  for (let i = 1; i <= numberOfDays; i++) {
+    let calID = dayOne + i;
+    if (i === numberOfDays) lastDay = calID;
+    if (i === todayDate) todayPlace = calID; 
+    let el = '#cal-' + calID.toString();
+    $(el).html(i.toString()).textFit({reProcess: true, alignVert: true, alignHoriz: true});
+    if ($(el).hasClass("today")) $(el).removeClass("today");
+    $(el).addClass("not-today");
+  }
+  $(".day-name").css("color", "#4FC1E8");
+  $("#day-label-" + todayDay.toString()).css("color", "#ED5564");
+  $('#cal-' + todayPlace.toString()).removeClass("not-today").addClass("today");
+  if (lastDay < 36) $("#week-6").css("height", "0%");
+  else $("#week-6").css("height", "13%");
   let t = setTimeout(forecastWeather, 3600000); //1 hour
 }
